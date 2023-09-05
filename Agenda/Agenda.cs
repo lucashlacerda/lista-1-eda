78d5e9a6e1ce3743fa5ba4ca3cdfa2de66f9bc84
Lucas <contatollacerda@gmail.com>
2023-09-05 15:21:18 -0300

    Criação do projeto da agenda

---
+++ Agenda/Agenda.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Agenda
+    {
+        List<Contato> listaDeContatos;
+
+        public Agenda()
+        {
+            listaDeContatos = new List<Contato>();
+        }
+        public int QtdContatos { get; set; }
+
+        public void AdicionarContato(Contato contato)
+        {
+            listaDeContatos.Add(contato);
+            QtdContatos++;
+        }
+
+        public string RemoverContato(Contato contato)
+        {
+            bool aux = false;
+
+            listaDeContatos.ForEach(c =>
+            {
+                if (c.Id == contato.Id)
+                {
+                    aux = true;
+                    listaDeContatos.Remove(c);
+                }
+            });
+
+            if (aux)
+            {
+                return "Contato removido com sucesso";
+            }
+            else
+                return "Não foi possível encontrar o contato";
+        }
+
+        public Contato BuscarContatoPorNome(string nome)
+        {
+            Contato contatoBuscado = new Contato();
+            listaDeContatos.ForEach(c =>
+            {
+                if(c.Nome == nome)
+                {
+                    contatoBuscado = c;
+                }
+            });
+            return contatoBuscado;
+        }
+    }
+}
---
+++ Agenda/Contato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    internal class Contato
+    {
+        private readonly int _id;
+
+        public Contato()
+        {
+            Random random = new Random();
+            _id = random.Next();
+        }
+
+        public int Id { get;  }
+        public string Nome { get; set; }
+        public string Email { get; set;}
+        public long Telefone { get; set; }  
+       
+    }
+}
