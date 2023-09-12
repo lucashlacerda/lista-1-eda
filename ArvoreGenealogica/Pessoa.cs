8a6067f4debf1d456e581705da38db3b7923bd28
Lucas <contatollacerda@gmail.com>
2023-09-12 12:29:30 -0300

    Iniciando árvore genealogica

---
+++ ArvoreGenealogica/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArvoreGenealogica
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public Pessoa Pai { get; set; }
+        public Pessoa Mae { get; set; }
+        public bool EIrmao(Pessoa pessoa) {
+            if(pessoa.Pai == this.Pai)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
